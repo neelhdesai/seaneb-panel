ea262c5c984837b546eca1829ab1e18b52253fa8
neelhdesai <neel.desai@seaneb.net>
2025-09-09 15:59:32 +0530

    Update ForgotPassword.jsx

--- frontend/src/components/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword.jsx
@@ -169,7 +169,7 @@ export default function ForgotPassword() {
                             <input
                                 type="text"
                                 value={otp}
-                                onChange={(e) => setOtp(e.target.value.replace(/\D/g, "").slice(0, 5))} // max 5 digits
+                                onChange={(e) => setOtp(e.target.value.replace(/\D/g, "").slice(0, 6))} // max 5 digits
                                 maxLength={5}
                                 className="peer w-full border-b-2 border-gray-300 bg-transparent py-2 focus:border-green-600 outline-none"
                                 placeholder=" "
@@ -285,3 +285,4 @@ export default function ForgotPassword() {
         </div>
     );
 }
+
