93ae36aceb70454e34fe96e86dd7478256c208c1
neelhdesai <neel.desai@seaneb.net>
2025-09-12 10:50:42 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { PendingConsultantsProvider } from "./context/PendingConsultantsContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <PendingConsultantsProvider>
+      <App />
+    </PendingConsultantsProvider>
   </StrictMode>,
 )
