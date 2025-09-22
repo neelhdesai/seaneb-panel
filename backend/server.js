48dcc690364c1eff9008a1ef55035c3f643fa37e
neelhdesai <neel.desai@seaneb.net>
2025-09-22 14:40:28 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,9 +50,10 @@ app.use("/api/cashfreepan", cashfreePanRoutes);
 
 // Server Listen
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on http://0.0.0.0:${PORT}`);
 });
+
 
 
 
