79b5a7d6aab177b5a20732237d9367db497068b0
neelhdesai <neel.desai@seaneb.net>
2025-09-22 09:36:42 +0530

    Update panRoutes.js

--- backend/routes/panRoutes.js
+++ backend/routes/panRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { verifyPan } from "../controllers/panController.js";
-import {initiatePanVerification, advancePanVerification} from "../controllers/cashfreepan.js"
+
 
 const router = express.Router();
 
-router.post("/verify-pan", verifyPan)
-router.post("/advance", advancePanVerification); 
+router.post("/verify", verifyPan)
 
 export default router;
+
 
