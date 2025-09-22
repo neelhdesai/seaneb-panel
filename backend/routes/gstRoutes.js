8373821a532168d2104e77578d6b182535089781
neelhdesai <neel.desai@seaneb.net>
2025-09-22 14:49:26 +0530

    Create gstRoutes.js

---
+++ backend/routes/gstRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { verifyGSTWithCashfree } from "../controllers/cashfreeGstController.js";
+
+const router = express.Router();
+
+// GST verification
+router.post("/verify-gst", verifyGSTWithCashfree);
+
+export default router;
